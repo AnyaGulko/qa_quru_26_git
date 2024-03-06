05a2225441d9d90263b461a201f51fb1289d1293
Anna Gulko <anya9inger@gmail.com>
2024-03-06 20:20:31 +0300

    Init commit

---
+++ src/main/java/ru/ingver/Main.java
@@ -0,0 +1,15 @@
+package ru.ingver;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int one = 2;
+        int second = 4;
+        int sum = one + second;
+
+        System.out.println(sum);
+    }
+}
